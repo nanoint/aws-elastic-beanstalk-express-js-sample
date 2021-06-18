adf5735ceb4442ae5f737dd28b0a876547b1a582
Nursultan Agzhigitov <nursultan.agzhigitov@gmail.com>
2021-06-19 01:04:25 +0200

    testing the pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello CloudFormation'));
+app.get('/', (req, res) => res.send('First AWS CI/CD Pipeline! It is not bad though, is it?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
